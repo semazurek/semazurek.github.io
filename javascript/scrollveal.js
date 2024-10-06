72006f30bc8558ad493fea34894229b547f54264
Sebastian Mazurek <85984736+semazurek@users.noreply.github.com>
2024-10-06 10:30:53 +0200

    Update scrollveal.js

--- javascript/scrollveal.js
+++ javascript/scrollveal.js
@@ -81,3 +81,16 @@ function GetEMAIL()
 		document.getElementById('label-email').innerHTML = 'sebastianmazurek@pm.me';
 	    	document.getElementById('button-email').innerHTML = "E-mail Copied";
     }
+var typed = new Typed('#typed', {
+    strings: ['Hello there!', 'How are you?'],
+    typeSpeed: 35,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 35,
+    // fadeOut: true,
+    loop: true,
+    loopCount: 2,
+    // showCursor: false
+    // cursorChar: '...'
+
+});
