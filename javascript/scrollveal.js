d46b63e88983e747e2f6f51c67493d4e292ff878
Sebastian Mazurek <85984736+semazurek@users.noreply.github.com>
2024-10-06 11:29:53 +0200

    Update scrollveal.js

--- javascript/scrollveal.js
+++ javascript/scrollveal.js
@@ -82,10 +82,10 @@ function GetEMAIL()
 	    	document.getElementById('button-email').innerHTML = "E-mail Copied";
     }
 var typed = new Typed('#typed', {
-    strings: ['I am CCNA, C#, PowerShell enthusiast. ', 'I am CCNA, C#, PowerShell enthusiast. I hold a security clearance for confidential and secret.', 'I am CCNA, C#, PowerShell enthusiast. I hold a security clearance for confidential and secret. After work I enjoy doing my own IT projects.'],
+    strings: ['Hello there!', 'I am CCNA, C#, PowerShell enthusiast. ', 'I am CCNA, C#, PowerShell enthusiast. I hold a security clearance for confidential and secret.', 'I am CCNA, C#, PowerShell enthusiast. I hold a security clearance for confidential and secret. After work I enjoy doing my own IT projects.'],
     typeSpeed: 35,
     startDelay: 2000,
-    backDelay: 1000,
+    //backDelay: 1000,
     backSpeed: 35,
     // fadeOut: true,
     //loop: true,
